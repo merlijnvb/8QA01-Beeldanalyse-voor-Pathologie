4104a013371f04e89af1cfa4004500fa79ecaccf
Merlijn van Benthem <74921668+merlijnvb@users.noreply.github.com>
2020-11-30 22:44:27 +0100

    Update melanoma_evaluator.py

--- melanoma_evaluator.py
+++ melanoma_evaluator.py
@@ -1,7 +1,10 @@
 import cv2
-import symmetry_function as sym
 import image_conversion as conv
+
+import symmetry_function as sym
+import border_function as bor
 import colour_function as col
+import diameter_function as dia
 
 lesion = cv2.imread("Melanoma.png") #can be any cut out of lesion
 lesion = cv2.cvtColor(lesion, cv2.COLOR_BGR2RGB)
@@ -12,6 +15,7 @@ ret,mask = cv2.threshold(gray_image,0,255,0)
 lesion_mask_rotated = conv.image_rotation(lesion,mask)[0]
 lesion_image_rotated = conv.image_rotation(lesion,mask)[1]
 
-print("colour score = ",col.colour_evaluation(lesion_image_rotated, lesion_mask_rotated)[0])
-    
-print("symmetry score = ",sym.symmetry_evaluation(lesion_image_rotated, lesion_mask_rotated)[0] + sym.symmetry_evaluation(lesion_image_rotated, lesion_mask_rotated)[1])
+print("(A) symmetry score =",sym.symmetry_evaluation(lesion_image_rotated, lesion_mask_rotated)[0] + sym.symmetry_evaluation(lesion_image_rotated, lesion_mask_rotated)[1])
+print("(B) border score =", bor.border_evaluation(lesion_mask_rotated)[0])
+print("(C) color score =",col.colour_evaluation(lesion_image_rotated, lesion_mask_rotated)[0])
+print("(D) diameter score =", dia.diameter_evaluation(lesion_mask_rotated))
