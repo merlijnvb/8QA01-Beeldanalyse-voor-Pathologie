289af248ff8b49f471ea46bbc032c9ee7a7027d5
Merlijn van Benthem <74921668+merlijnvb@users.noreply.github.com>
2020-12-17 18:27:55 +0100

    Update look_at_this_graphh.py

--- Merlijn/look_at_this_graphh.py
+++ Merlijn/look_at_this_graphh.py
@@ -1,21 +1,16 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import os
 
 """READ IMAGES AND CONVERT THEM"""
 def img_conversion(mask_file,lesion_file):
     mask = cv2.imread(mask_file, cv2.IMREAD_GRAYSCALE)
  
     lesion = cv2.imread(lesion_file)
-    lesion = cv2.cvtColor(lesion, cv2.COLOR_BGR2RGB)
     lesion = cv2.bitwise_and(lesion, lesion, mask=mask)
     
-    plt.imshow(mask)
-    plt.suptitle("Original Mask")
-    plt.show()
-    plt.imshow(lesion)
-    plt.suptitle("Original Lesion")
-    plt.show()
+    
+    cv2.imwrite("original_lesion.png", lesion)
     
     height, width = mask.shape[:2]
     centre = (width // 2, height // 2)
@@ -28,15 +23,13 @@ def img_conversion(mask_file,lesion_file):
     
     mask = cv2.warpAffine(mask, moment, (width, height))
     lesion = cv2.warpAffine(lesion, moment, (width, height))
+
+    cv2.imwrite("rotated_mask.png", mask)
+    cv2.imwrite("rotated_lesion.png", lesion)
+
+    lesion = cv2.cvtColor(lesion, cv2.COLOR_BGR2RGB)
     
-    plt.imshow(mask)
-    plt.suptitle("Rotated Mask")
-    plt.show()
-    plt.suptitle("Rotated Lesion")
-    plt.imshow(lesion)
-    plt.show()    
-    
-    return (mask, lesion)
+    return (mask,lesion)
 
 """EVALUATE LESIONS"""
 def border_evaluation(mask):       
@@ -47,18 +40,11 @@ def border_evaluation(mask):
         
     border = cv2.drawContours(border_blanc,contours, 0, (255, 255, 255), 2)
     
-    length_border = np.sum(border == 255)
-    
-    plt.imshow(border)
-    plt.suptitle("Border")
-    plt.show()   
-    
-    return length_border
+    os.remove("border.png")
+    cv2.imwrite("border.png", border)
     
 def colour_evaluation(mask, lesion):  
     mask_inv = 255 - mask
-    area = np.sum(mask == 255)
-    colour_score = 0
     
     colours = {'light brown low':(255*0.588, 255*0.2, 255*0),
               'light brown high':(255*0.94, 255*0.588, 255*392),
@@ -79,14 +65,11 @@ def colour_evaluation(mask, lesion):
         if list(colours.keys())[i] == list(colours.keys())[-2] and list(colours.keys())[i+1] == list(colours.keys())[-1]:
             mask_colour = mask_colour - mask_inv
         
-        plt.imshow(mask_colour)
-        plt.suptitle("colour range: {}".format([i]))
-        plt.show() 
+        mask_in_colour = cv2.bitwise_and(lesion, lesion, mask=mask_colour)
+        mask_in_colour = cv2.cvtColor(mask_in_colour, cv2.COLOR_RGB2BGR)
         
-        if (np.sum(mask_colour == 255) / area) >= 0.05:
-            colour_score += 1
-        
-    return (area,colour_score)
+        os.remove("coloured_mask_{}.png".format(i))
+        cv2.imwrite("coloured_mask_{}.png".format(i), mask_in_colour)
     
 def symmetry_evaluation(mask):
     height, width = mask.shape[:2]
@@ -124,36 +107,23 @@ def symmetry_evaluation(mask):
     
     if superior.shape[0] == inferior.shape[0]:
         horizontal_result = superior - inferior
-        
-    """SIDES"""
-    plt.imshow(inferior)
-    plt.suptitle("inferior side")
-    plt.show()
-    plt.imshow(superior)
-    plt.suptitle("superior side")
-    plt.show()
-    plt.imshow(left)
-    plt.suptitle("left side")
-    plt.show()
-    plt.imshow(right)
-    plt.suptitle("right side")
-    plt.show()
-    """OVERLAPSE"""
-    plt.imshow(horizontal_result)
-    plt.suptitle("Horizontal overlapse")
-    plt.show()
-    plt.suptitle("Vertical overlapse")
-    plt.imshow(vertical_result)
-    plt.show() 
-        
-    horizontal_result = np.sum(horizontal_result == 255)
-    vertical_result = np.sum(vertical_result == 255)
     
-    return (horizontal_result,vertical_result)
+    os.remove("inferior_side.png")
+    os.remove("superior_side.png")
+    os.remove("left_side.png")
+    os.remove("right_side.png")
+    os.remove("horizontal_overlapse.png")
+    os.remove("vertical_overlapse.png")
+    cv2.imwrite("inferior_side.png", inferior)
+    cv2.imwrite("superior_side.png", superior)
+    cv2.imwrite("left_side.png", left)
+    cv2.imwrite("right_side.png", right)
+    cv2.imwrite("horizontal_overlapse.png", horizontal_result)
+    cv2.imwrite("vertical_overlapse.png", vertical_result)
 
 """CALL EVERY FUNCTION 1 TIME TO SHOW RESULTS"""
 def return_results(mask_file, lesion_file):
-    mask, lesion = img_conversion(mask_file, lesion_file)   
+    mask, lesion = img_conversion(mask_file, lesion_file)
     
     symmetry_evaluation(mask)
     border_evaluation(mask)
