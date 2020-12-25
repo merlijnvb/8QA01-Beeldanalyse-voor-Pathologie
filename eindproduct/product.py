1d9980be60ce1f4be7ad1927666b2f949124dd4c
Merlijn van Benthem <74921668+merlijnvb@users.noreply.github.com>
2020-12-25 19:05:55 +0100

    Add files via upload

---
+++ eindproduct/product.py
@@ -0,0 +1,190 @@
+import cv2
+import os
+import numpy as np
+from tqdm import tqdm
+from sklearn.utils import shuffle
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+
+
+"""
+    READ ALL FILES IN MAPS
+"""
+def read_files():
+    lesion_list = []
+    
+    for file in os.listdir("ISIC-TrainValTest\lesions"):
+        if file.endswith(".jpg"):
+            lesion_list.append(("{}_segmentation.png".format(file[:file.index(".")]),"{}.jpg".format(file[:file.index(".")])))
+    
+    return lesion_list
+
+
+"""
+    READ IMAGES AND CONVERT THEM
+"""
+def img_conversion(mask_file,lesion_file):
+    mask = cv2.imread(mask_file, cv2.IMREAD_GRAYSCALE)
+ 
+    lesion = cv2.imread(lesion_file)
+    lesion = cv2.cvtColor(lesion, cv2.COLOR_BGR2RGB)
+    lesion = cv2.bitwise_and(lesion, lesion, mask=mask)
+    
+    height, width = mask.shape[:2]
+    centre = (width // 2, height // 2)
+    
+    thresh = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)[1]    
+    contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
+    
+    try:
+        angle = cv2.fitEllipse(contours[0])[2] - 90
+        
+    except:
+        angle = 0
+
+    moment = cv2.getRotationMatrix2D(centre, angle, 1.0)
+    
+    mask = cv2.warpAffine(mask, moment, (width, height))
+    lesion = cv2.warpAffine(lesion, moment, (width, height))
+
+    return (mask, lesion)
+
+
+"""
+    EVALUATE LESIONS
+"""
+def border_evaluation(mask):       
+    border_blanc = np.zeros((mask.shape[0], mask.shape[1], 3), np.uint8)
+    
+    thresh = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)[1]    
+    contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
+        
+    border = cv2.drawContours(border_blanc,contours, 0, (255, 255, 255), 3)
+    
+    length_border = np.sum(border == 255)
+
+    return length_border
+    
+def colour_evaluation(mask, lesion):  
+    mask_inv = 255 - mask
+    area = np.sum(mask == 255)
+    colour_score = 0
+    
+    colours = {'light brown low':(255*0.588, 255*0.2, 255*0),
+              'light brown high':(255*0.94, 255*0.588, 255*392),
+              'dark brown low':(255*0.243, 255*0, 255*0),
+              'dark borwn high':(255*56, 255*0.392, 255*392),
+              'white low':(255*0.8, 255*0.8, 255*0.8),
+              'white high':(255, 255, 255),
+              'red low':(255*0.588, 255*0, 255*0),
+              'red high':(255, 255*0.19, 255*0.19),
+              'blue gray low':(255*0, 255*0.392, 255*0.490),
+              'blue gray high':(255*0.588, 255*0.588, 255*0.588),
+              'black low':(255*0, 255*0, 255*0),
+              'black high':(255*0.243, 255*0.243, 255*0.243)}
+    
+    for i in range(0,len(colours),2):
+        mask_colour = cv2.inRange(lesion, colours.get(list(colours.keys())[i]), colours.get(list(colours.keys())[i+1]))
+    
+        if list(colours.keys())[i] == list(colours.keys())[-2] and list(colours.keys())[i+1] == list(colours.keys())[-1]:
+            mask_colour = mask_colour - mask_inv
+        
+        if (np.sum(mask_colour == 255) / area) >= 0.05:
+            colour_score += 1
+        
+    return (area,colour_score)
+
+def color_cluster_evaluation(lesion, clusters = 5):
+    lesion = cv2.cvtColor(lesion, cv2.COLOR_RGB2HSV)
+    
+    w, h, d = lesion.shape[:]
+    image_array = np.reshape(lesion, (w * h, d)) 
+    image_array_sample = shuffle(image_array, random_state=0)[:10000] 
+    
+    kmeans = KMeans(n_clusters=clusters, random_state=0).fit(image_array_sample)
+    centroids = kmeans.cluster_centers_
+    
+    D = cdist(centroids, centroids, metric='seuclidean')
+    total = 0
+    
+    for i in range(clusters):
+        for j in range(clusters):
+            if i<j:
+                total = total + D[i, j]
+
+    mean_dinstance = total/((clusters*(clusters-1))/2)
+
+    return mean_dinstance
+    
+def symmetry_evaluation(mask):
+    height, width = mask.shape[:2]
+    moment = cv2.moments(mask)
+    
+    centre_blob_x = int(moment["m10"] / moment["m00"])
+    centre_blob_y = int(moment["m01"] / moment["m00"])
+
+    superior = mask[0:centre_blob_y, 0:width]
+    inferior = mask[centre_blob_y:height, 0:width]
+    inferior = cv2.flip(inferior, 0)
+    
+    left = mask[0:height, 0:centre_blob_x]
+    left = cv2.flip(left, 1)
+    right = mask[0:height, centre_blob_x:width]
+    
+    if superior.shape[0] > inferior.shape[0]:
+        inferior = cv2.copyMakeBorder(inferior, superior.shape[0]-inferior.shape[0], None, None, None, 0, None, None)                     
+        horizontal_result = superior - inferior
+        
+    if superior.shape[0] < inferior.shape[0]:
+        superior = cv2.copyMakeBorder(superior, inferior.shape[0]-superior.shape[0], None, None, None, 0, None, None)  
+        horizontal_result = inferior - superior
+    
+    if left.shape[1] > right.shape[1]:
+        right = cv2.copyMakeBorder(right, None, None, None, left.shape[1]-right.shape[1], 0, None, None)
+        vertical_result = left - right
+        
+    if left.shape[1] < right.shape[1]:
+        left = cv2.copyMakeBorder(left, None, None, None, right.shape[1]-left.shape[1], 0, None, None)
+        vertical_result = right - left
+        
+    if left.shape[1] == right.shape[1]:  
+        vertical_result = right - left
+    
+    if superior.shape[0] == inferior.shape[0]:
+        horizontal_result = superior - inferior
+        
+    horizontal_result = np.sum(horizontal_result == 255)
+    vertical_result = np.sum(vertical_result == 255)
+    
+    return (horizontal_result,vertical_result)
+
+
+"""
+    RETURN RESULTS OF LESIONS
+"""
+def return_results():
+    #OPEN DOCUMENT TO INSERT RESULTS
+    document = open("results.csv", "w")
+    data = read_files()
+    
+    for fileset in tqdm(data):
+        index = fileset[1][:-4]
+        
+        if index == data[0][1][:-4]:
+            document.write("index,lng_bor,area,hor_overl,vrt_overl,clr_score,cluster_clr_score\n")
+        
+        mask_file = "ISIC-TrainValTest\masks\{}".format(fileset[0])
+        lesion_file = "ISIC-TrainValTest\lesions\{}".format(fileset[1])
+        
+        mask, lesion = img_conversion(mask_file, lesion_file)
+        
+        lng_bor = border_evaluation(mask)
+        hor_overl, vrt_overl = symmetry_evaluation(mask)
+        area, clr_score = colour_evaluation(mask, lesion)
+        cluster_clr_score = color_cluster_evaluation(lesion)
+        
+        line = "{0:s},{1:d},{2:d},{3:d},{4:d},{5:d},{6:f}\n".format(index,lng_bor,area,hor_overl,vrt_overl,clr_score,cluster_clr_score)
+        document.write(line)
+    document.close()
+
+return_results()
---
+++ eindproduct/score.py
@@ -0,0 +1,179 @@
+import math
+import pandas as pd
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neighbors import NearestCentroid
+
+def read_files():
+    csv = open("labels.csv")
+    csv_read = csv.readlines()
+    csv.close()
+    results = open("results.csv")
+    results_read = results.readlines()
+    results.close()
+    
+    control_groups = []
+    value_data = []
+    
+    for lines in csv_read[1:]:
+        lines = tuple(lines.rstrip().split(",")[:-1])
+        control_groups.append(lines)
+    
+    for lines in results_read[1:]:
+        lines = lines.rstrip()
+        lines = tuple(lines.split(","))
+        value_data.append(lines)
+    
+    control_groups.sort()
+    value_data.sort()
+    
+    return value_data, control_groups
+
+def extract_info(value_data, control_groups):
+    
+    list_id = []
+    list_data = []
+    
+    for tupl in value_data:
+        ID = tupl[0]
+        border = int(tupl[1])
+        area = int(tupl[2])
+        symmetry_vertical = int(tupl[3])
+        symmetry_horizontal = int(tupl[4])
+        colour_score = int(tupl[5])
+        cluster_colour_score = float(tupl[6])
+        
+        diameter = border / math.pi
+        
+        border_score = (border**2) / (area*math.pi*4)
+        symmetry_score = (symmetry_vertical + symmetry_horizontal) / area
+        diameter_score = diameter / area
+        
+        list_id.append(ID)
+        list_data.append((symmetry_score,border_score,colour_score,cluster_colour_score,diameter_score))
+    
+    df = pd.DataFrame(list_data,index=list_id,columns=["Asymmetry score","Border score","Colour score","Cluster score","Diameter score"])
+    
+    indexes = []
+    control_list = []
+    
+    for data in control_groups:
+        indexes.append(data[0])
+        control_list.append(data[1])
+           
+    for id in df.index:
+        if id not in indexes:
+            df = df.drop(index=id)
+
+    return df, control_list
+
+def print_accuracy(test_features,control_group,folds,classifiers):
+    x_train, x_test, y_train, y_test = train_test_split(test_features, control_group,test_size=100/len(control_group), random_state=folds)
+     
+    scaler = MinMaxScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    logreg = LogisticRegression()
+    clf2 = DecisionTreeClassifier(max_depth=3).fit(x_train, y_train)
+    knn = KNeighborsClassifier()
+    gnb = GaussianNB()
+    lda = LinearDiscriminantAnalysis()
+    svm = SVC()
+    cent = NearestCentroid()
+    
+    logreg.fit(x_train, y_train)
+    knn.fit(x_train, y_train)   
+    gnb.fit(x_train, y_train)
+    lda.fit(x_train, y_train)    
+    svm.fit(x_train, y_train)
+    cent.fit(x_train, y_train)
+    
+    def get_accuracy(x,y):
+        a = logreg.score(x, y)
+        b = clf2.score(x, y)
+        c = knn.score(x, y)
+        d = gnb.score(x, y)
+        e = lda.score(x, y)
+        f = svm.score(x, y)
+        g = cent.score(x, y)
+        
+        return (a,b,c,d,e,f,g)
+
+    training_sets = []
+    test_sets = []
+    
+    for i in range(len(classifiers)):
+        train = float(get_accuracy(x_train,y_train)[i])
+        test = float(get_accuracy(x_test, y_test)[i])
+        
+        training_sets.append(train)
+        test_sets.append(test)
+       
+    training_sets = tuple(training_sets)
+    test_sets = tuple(test_sets)
+    
+    return (training_sets,test_sets)
+
+def define_score(features):    
+    value_data, control_groups = read_files()
+    df, control_list = extract_info(value_data,control_groups)
+    
+    classifiers = ["Logistic regression","Decision Tree","Nearest Neighbor"," Linear Discriminant Analysis",
+                   "Gaussian Naive Bayes","Support Vector Machine","Nearest Centroid"]
+    
+    features = features[1]
+    name = features[0]
+    test_features = df[features]
+        
+    iteration = []
+
+    for folds in tqdm(range(len(df))):
+        iteration.append(print_accuracy(test_features,control_list,folds,classifiers))
+   
+    data = []
+    
+    for mode in range(2):  
+        for types in range(len(classifiers)):
+            value = 0
+            for dataset in iteration:
+                value += dataset[mode][types]
+                
+            value = value / len(iteration)
+            
+            data.append(value)
+    
+    mean_train = []
+    mean_test = []
+    
+    for train in data[:7]:
+        train = "{:0.2%}".format(train)
+        mean_train.append(train)
+        
+    for test in data[7:]:
+        test = "{:0.2%}".format(test)
+        mean_test.append(test)
+    
+    mean_table = pd.DataFrame({"Types of classification:":classifiers,
+                        "Mean training:":mean_train,
+                        "Mean test:":mean_test})
+    
+    mean_table = mean_table.to_csv("classifiers_{}.csv".format(name),index=False,sep=",")
+
+def get_results():
+    features = [("intervals",["Asymmetry score","Border score","Colour score","Diameter score"]),
+                ("clusters",["Asymmetry score","Border score","Cluster score","Diameter score"])]
+    
+    for feature in features:
+        define_score(feature)
+
+get_results()
+    
+    
